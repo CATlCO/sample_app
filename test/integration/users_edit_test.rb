bbb98405570a20051c107353c4600f66adea0679
kathykate <kathy0kate@gmail.com>
2015-11-20 16:35:15 +0100

    finish user edit, update, index, destroy

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@user = users(:michael)
+	end
+  
+	test 'invalid edit information' do
+		log_in_as @user
+		get edit_user_path(@user)
+		assert_template 'users/edit'
+		patch user_path(@user), user: {name: "", email: "user@invalid", password: "foo", password_confirmation: "bar"}
+		assert_template 'users/edit'
+	end
+
+	test 'valid edit information with friendly forwarding' do
+		name = "ex"
+		email = "user@ex.com"
+		get edit_user_path(@user)
+		assert_redirected_to login_path #*** might be removed
+		log_in_as @user
+		assert_redirected_to edit_user_path(@user)
+		patch user_path(@user), user: {name: name, email: email, password: "", password_confirmation: ""}
+		assert flash[:success]
+	  assert_redirected_to @user
+	  @user.reload
+	  assert_equal name, @user.name
+	  assert_equal email, @user.email
+	end
+end
---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+  def setup
+  	@admin = users(:michael)
+  	@another = users(:archer)
+  end
+
+  test 'index includes pagination and delete links for admin' do
+  	log_in_as(@admin)
+  	get users_path
+  	assert_template 'users/index'
+  	assert_select 'div.pagination'
+  	User.paginate(page: 1).each do |user|
+  		assert_select 'a[href=?]', user_path(user), text: user.name
+  		assert_select 'a[href=?]', user_path(user), text: 'delete' unless user == @admin
+  	end
+  end
+
+  test 'admin should be able to delete user' do
+  	log_in_as(@admin)
+  	assert_difference 'User.count', -1 do
+  		delete user_path(@another)
+  	end
+  	assert_redirected_to users_path
+  end
+
+  test 'index should not include delete links when logged as non-admin' do
+  	log_in_as(@another)
+  	get users_path
+  	assert_select 'a', text: 'delete', count: 0
+  end
+
+end
