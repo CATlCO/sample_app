ff9ff59b81b4281a36459839140e45d72c847552
kathykate <kathy0kate@gmail.com>
2015-11-23 20:36:05 +0100

    Account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+	def setup
+		ActionMailer::Base.deliveries.clear
+	end
+
 	test 'invalid signup information' do
 		get signup_path
 		assert_no_difference 'User.count' do
@@ -16,10 +20,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 	test 'valid signup information' do
 		get signup_path
 		assert_difference 'User.count', 1 do
-			post_via_redirect users_path, user: {name: "ex", email: "user@ex.com", password: "foobar", password_confirmation: "foobar"}
+			post users_path, user: {name: "ex", email: "user@ex.com", password: "foobar", password_confirmation: "foobar"}
 		end
+		assert_equal 1, ActionMailer::Base.deliveries.size
+		user = assigns(:user)
+		assert_not user.activated?
+		log_in_as user
+		assert_not is_logged_in?
+		get edit_account_activation_path("invalid token")
+		assert_not is_logged_in?
+		get edit_account_activation_path(user.activation_token, email: "wrong")
+		assert_not is_logged_in?
+		get edit_account_activation_path(user.activation_token, email: user.email)
+		assert user.reload.activated?
+		follow_redirect!
 		assert is_logged_in?
 		assert_template 'users/show'
-		assert flash[:success]
 	end
 end
